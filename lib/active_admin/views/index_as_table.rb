048c74b47e06f7731727b66f534e5077841a601f
lukas andrade <lukas.andrade@bemind.com.br>
2011-07-05 11:21:48 -0300

    Alteração do módulo DEFAULT_ACTIONS

--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -38,13 +38,15 @@ module ActiveAdmin
           }.merge(options)
           column options[:name] do |resource|
             links = ''
-            if resource.ativo
+            if (resource.respond_to?('ativo') && resource.ativo) or (resource.respond_to?('status') && resource.status == "ativo") or
+                !resource.respond_to?('ativo') or !resource.respond_to('status')
               links = link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
               links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
               links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
             else
               links += link_to I18n.t('active_admin.activate'), activate_resource_path(resource), :method => :put, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link activate_link"
             end
+            
             links.html_safe
           end
         end
