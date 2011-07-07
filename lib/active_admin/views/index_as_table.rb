08656292c33c8dda1da3a9d061c86d71996669d2
lukas andrade <lukas.andrade@bemind.com.br>
2011-07-07 08:56:15 -0300

    Desfazendo alterações nas dafault_actions

--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -38,8 +38,7 @@ module ActiveAdmin
           }.merge(options)
           column options[:name] do |resource|
             links = ''
-            if (resource.respond_to?('ativo') && resource.ativo) or (resource.respond_to?('status') && resource.status == "ativo") or
-                !resource.respond_to?('ativo') or !resource.respond_to('status')
+            if (resource.ativo)
               links = link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
               links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
               links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
